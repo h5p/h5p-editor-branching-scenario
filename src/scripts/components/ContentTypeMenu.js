9f4669d126ec0a825ca65f8e974fc747bf14ac65
timothyylim <timothylim23@gmail.com>
2018-04-19 16:51:49 +0200

    HFP-1571 Add ability to drag

--- src/scripts/components/ContentTypeMenu.js
+++ src/scripts/components/ContentTypeMenu.js
@@ -3,6 +3,28 @@ import './ContentTypeMenu.scss';
 import Tooltip from './Tooltip';
 
 export default class ContentTypeMenu extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.handleMouseDown = this.handleMouseDown.bind(this);
+  }
+
+  handleMouseDown(e, data) {
+    const positionData = this.refs.contentTypeButton.getBoundingClientRect();
+
+    const mouseDownData = {
+      contentClass: e.currentTarget.className,
+      xPos: positionData.x,
+      yPos: positionData.y,
+      width: positionData.width,
+      height: positionData.height,
+      top: positionData.top
+    }
+    this.props.onMouseDown(e, mouseDownData);
+    e.stopPropagation();
+    e.preventDefault();
+  }
+
   render() {
     return (
       <div className="content-type-menu">
@@ -13,12 +35,12 @@ export default class ContentTypeMenu extends React.Component {
           </Tooltip>
         </label>
         <ul className="content-type-buttons">
-          <li className="video blue" title="Add New Video">Video</li>
-          <li className="presentation blue" title="Add New Course Presentation">Presentation</li>
-          <li className="text blue" title="Add New Text">Text</li>
-          <li className="image blue" title="Add New Image">Image</li>
-          <li className="interactive-video blue" title="Add New Interactive Video">Interac. Video</li>
-          <li className="image-hotspots blue" title="Add New Image Hotspots">Hotspots</li>
+        <li className="video" title="Add New Video" ref={"contentTypeButton"} onMouseDown={this.handleMouseDown} onMouseUp={this.props.onMouseUp}>Video</li>
+          <li className="presentation" title="Add New Course Presentation">Presentation</li>
+          <li className="text" title="Add New Text">Text</li>
+          <li className="image" title="Add New Image">Image</li>
+          <li className="interactive-video " title="Add New Interactive Video">Interac. Video</li>
+          <li className="image-hotspots" title="Add New Image Hotspots">Hotspots</li>
         </ul>
         <label className="label-info">
           Branching Content
