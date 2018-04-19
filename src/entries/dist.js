83f17fa137b8bb0214f2568747120bb570baf2a0
Oliver Tacke <o.tacke@posteo.de>
2018-04-19 17:28:21 +0200

    HFP-1653 Add (preliminary) function to get common fields from semantics
    Semantics of main content and subcontent can be parsed for "common" fields
    (or other field combinations, since the function is general).
    The idea was to not touch the core, but this way all libraries will have
    to be loaded and it might take 3-4 seconds until the interface for
    all translations is fully populated.
    
    It's propably better to find a way to grab the required information by
    modifying h5peditor.js in core.

--- src/entries/dist.js
+++ src/entries/dist.js
@@ -21,6 +21,182 @@ H5PEditor.widgets.branchingScenario = H5PEditor.BranchingScenario = (function ($
     this.params = params || {};
     setValue(field, this.params);
 
+    this.translations = [];
+
+    /**
+     * Get all the machine names of content types used.
+     *
+     * @param {object} [params] - Parameters.
+     * @return {object[]} Array of machine names of content types used.
+     */
+    const getLibraryNames = function (params = {}, results = []) {
+      if (!Array.isArray(results) || results.some(result => typeof result !== 'string')) {
+        return [];
+      }
+
+      if (!params.content || !Array.isArray(params.content)) {
+        return results;
+      }
+
+      params.content.forEach(content => {
+        if (!content.content || typeof content.content !== 'object') {
+          return;
+        }
+        if (content.content.library && typeof content.content.library === 'string' && !results.includes(content.content.library)) {
+          results.push(content.content.library);
+        }
+      });
+
+      return results;
+    };
+
+    /**
+     * Flatten semantics. Unsanitized.
+     *
+     * @param {object} field - Semantics field to start with flattening.
+     * @return {object[]} Flattened semantics.
+     */
+    const flattenSemantics = function(field) {
+      if (!Array.isArray(field)) {
+        field = [field];
+      }
+
+      let results = [];
+
+      field.forEach(field => {
+        results.push(field);
+        if (field.type === 'group') {
+          results = results.concat(flattenSemantics(field.fields));
+        }
+        if (field.type === 'list') {
+          results.push(field.field);
+          results = results.concat(flattenSemantics(field.field.fields));
+        }
+      });
+
+      return results;
+    };
+
+    /**
+     * Check if field properties match a filter in a given way.
+     *
+     * @param {object} field - Semantics field.
+     * @param {object} filter - Filter.
+     * @param {string} filter.property - Field property to compare with.
+     * @param {number|string|boolean} filter.value - Property value to compare with.
+     * @param {string} filter.mode - Comparator between properties, e.g. "===" or "!==".
+     * @return {boolean} True, if property matches value given the mode.
+     */
+    const fieldMatchesFilter = function(field, filter) {
+      if (!field || !filter || typeof filter.value === 'undefined') {
+        return false;
+      }
+
+      if (!filter.mode) {
+        filter.mode = '===';
+      }
+
+      // More comparators could be added
+      switch (filter.mode) {
+        case '===':
+          return (field[filter.property] === filter.value);
+        case '!==':
+          return (field[filter.property] !== filter.value);
+      }
+
+      return false;
+    };
+
+    /**
+     * Check if particular comnbination of property filters is given.
+     *
+     * @param {object} field - Semantics field.
+     * @param {object[]} filters - List of filters.
+     * @param {string} filters[].property - Field property to compare with.
+     * @param {number|string|boolean} filters[].value - Property value to compare with.
+     * @param {string} filters[].[mode] - Comparator between properties, e.g. "===" (default) or "!==".
+     * @param {string} [mode] - Comparator between filters, e.g. "||" (default) or "&&".
+     * @return {boolean} True, if property combination matches mode.
+     */
+    const andOrOr = function (field, filters = {}, mode = '||') {
+      if (!field) {
+        return false;
+      }
+
+      switch (mode) {
+        case '||':
+          return filters.some(filter => fieldMatchesFilter(field, filter));
+        case '&&':
+          return filters.every(filter => fieldMatchesFilter(field, filter));
+      }
+
+      return false;
+    };
+
+    /**
+     * Filter semantics for particular property combinations.
+     *
+     * @param {object} field - Semantics field.
+     * @param {object[]} filters - List of filters.
+     * @param {string} filters[].property - Field property to compare with.
+     * @param {number|string|boolean} filters[].value - Property value to compare with.
+     * @param {string} filters[].[mode] - Comparator between properties, e.g. "===" (default) or "!==".
+     * @param {string} [mode] - Comparator between filters, e.g. "||" (default) or "&&".
+     */
+    const filterSemantics = function (field, filters = [], mode = '||') {
+      if (!Array.isArray(filters)) {
+        filters = [filters];
+      }
+
+      const flatSemantics = flattenSemantics(field);
+      return flatSemantics.filter(semantic => andOrOr(semantic, filters, mode));
+    };
+
+    // This is terribly slow! Maybe it's better to pull the common semantics fields from somewhere else?
+    const promise = new Promise((resolve, reject) => {
+      const libraryNames = getLibraryNames(this.params, [parent.currentLibrary]);
+      const allSemantics = [];
+      libraryNames.forEach(libraryName => {
+        H5PEditor.loadLibrary(libraryName, result => {
+          allSemantics.push({library: libraryName, semantics: {
+            type: 'group',
+            fields: result
+          }});
+          if (allSemantics.length === libraryNames.length) {
+            resolve(allSemantics);
+          }
+        });
+      });
+    });
+
+    promise.then((results) => {
+      results.forEach(result => {
+        // Can contain "common" group fields with further "common" text fields nested inside
+        const firstFilter = filterSemantics(result.semantics, {property: 'common', value: true});
+
+        // Flatten out the firstFilter to get a plain structure
+        this.translations[result.library] = filterSemantics(firstFilter, {property: 'type', value: 'text'});
+      });
+      /*
+       * TODO:
+       * this.translations now contains all translatable fields as
+       *
+       * [
+       *   {
+       *     "Library Machine Name x.y": [
+       *       {name: ...},
+       *       {name: ...}
+       *     ]
+       *   }
+       * ]
+       *
+       * BUT the procedure is too slow, and we still don't have the values from params and would have
+       * to get them, too. There must be a smarter way!
+       */
+      console.log(this.translations);
+    });
+
+
     // TODO: Match semantics with design
     // TODO: Sanitized access more elegantly
     this.params.startScreen = this.params.startScreen || {};
