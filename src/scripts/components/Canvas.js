3519ff578812001d00a84ba240bc2d0afa04eca3
Oliver Tacke <o.tacke@posteo.de>
2018-09-25 13:05:33 +0200

    HFP-2237 Open edit form on double click on Draggable
    We could also use jQuery's dblClick listener, but we
    cannot know what the double click speed of the OS is.

--- src/scripts/components/Canvas.js
+++ src/scripts/components/Canvas.js
@@ -146,10 +146,17 @@ export default class Canvas extends React.Component {
       this.props.onDropped(); // TODO: Determine if should really run after set state. Note that this triggers a changing in the props which sets the state again through componentWillReceiveProps, which is deprected. Can we find a better way of doing this?
       // I guess only the parent should keep track of this state? yes
     }
-    else {
+    else if (this.state.placing !== id || id === -1) {
       // Start placing
       this.setState({
         placing: id
+      });
+    }
+    else {
+      // Start editing
+      this.setState({
+        placing: null,
+        editing: id
       });
     }
   }
