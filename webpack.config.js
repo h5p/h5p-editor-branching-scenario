01eeaf5dd354d368f0105882c6216703fecf444e
Zouhair Loucif <zouhair.loucif@gmail.com>
2022-07-04 12:36:52 +0200

    JI-3673 Cleanup for packages and replaced deprecated packages (#40)
    * JI-3673 Cleanup for packages and replaced deprecated packages
    
    * JI-3673 Modify webpack configuration
    
    * JI-3673 Keep using React 16
    
    Co-authored-by: Erik Langhaug <erik.langhaug@joubel.com>

--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,14 @@ var webpack = require('webpack');
 
 var nodeEnv = process.env.NODE_ENV || 'development';
 var isDev = (nodeEnv !== 'production');
+
 var config = {
+  mode: nodeEnv,
+  context: path.resolve(__dirname, 'src'),
   entry: {
-    dist: './src/entries/app.js'
+    dist: './entries/app.js'
   },
+  devtool: isDev ? 'inline-source-map' : undefined,
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].js'
@@ -21,20 +25,25 @@ var config = {
       {
         test:/\.(s*)css$/,
         include: path.resolve(__dirname, 'src'),
-        use: ['style-loader', 'css-loader', 'resolve-url-loader', 'sass-loader']
+        use: [
+          'style-loader',
+          'css-loader',
+          'resolve-url-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true
+            }
+          }
+        ]
       },
       {
         test: /\.(png|woff|woff2|eot|ttf|svg|gif)$/,
         include: path.resolve(__dirname, 'src'),
-        loader: 'url-loader?limit=1000000'
+        type: 'asset/resource',
       }
     ]
   }
 };
-
-if (isDev) {
-  config.devtool = 'inline-source-map';
-  // config.devtool = 'cheap-module-eval-source-map';
-}
 
 module.exports = config;
